0ec545ecc167ca6f5eb5a067dde7c81538a5f47c
Felicity M <oatstesting@gmail.com>
2017-08-13 08:54:18 +0530

    Resovled the conflicts.

--- TestGit/src/tests/LoginModule.java
+++ TestGit/src/tests/LoginModule.java
@@ -3,6 +3,7 @@ package tests;
 public class LoginModule {
 
 	public static void main(String args[]){
-		System.out.println("Welcome to my page");
+		System.out.println("Welcome to my page. Testing.");
+		System.out.println("Welcome to my page. Have a good day.");
 	}
 }
