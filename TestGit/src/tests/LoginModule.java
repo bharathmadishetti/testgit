f0342d047ea149740c2b0ba7577fbeb6c3cef7b8
Felicity M <oatstesting@gmail.com>
2017-08-15 14:03:10 +0530

    Modified

--- TestGit/src/tests/LoginModule.java
+++ TestGit/src/tests/LoginModule.java
@@ -3,7 +3,6 @@ package tests;
 public class LoginModule {
 
 	public static void main(String args[]){
-//testing
 	//Grapes	
 		System.out.println("Welcome to my page. ");
 		System.out.println("Lion");
