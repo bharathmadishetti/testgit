38d2c7ddbc2401dfe5acdc647ffb2526411f6ad7
bharathmadishetti <bharathmadishetti@gmail.com>
2017-08-15 14:03:50 +0530

    xxx
    xxxxx

--- TestGit/src/tests/LoginModule.java
+++ TestGit/src/tests/LoginModule.java
@@ -3,7 +3,6 @@ package tests;
 public class LoginModule {
 
 	public static void main(String args[]){
-	//Grapes	
 		System.out.println("Welcome to my page. ");
 		System.out.println("Lion");
 		System.out.println("Welcome to my page. Have a good day.");
