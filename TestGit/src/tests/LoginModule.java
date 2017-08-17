8e2d39c496509186e0abb7198c4845872e156f68
Felicity M <oatstesting@gmail.com>
2017-08-17 08:15:43 +0530

    Added comment to the project to tell this is demo purpose.

--- TestGit/src/tests/LoginModule.java
+++ TestGit/src/tests/LoginModule.java
@@ -4,6 +4,7 @@ public class LoginModule {
 
 	public static void main(String args[]){
 		//testing
+		//Testing project for demo purpose
 		System.out.println("Welcome to my page. ");
 		System.out.println("Lion");
 		System.out.println("Welcome to my page. Have a good day.");
